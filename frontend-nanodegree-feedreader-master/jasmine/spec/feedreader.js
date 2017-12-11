672327c2ae2a470812566710b1c803bbb7b6569c
bridget3 <bridgetfagan3@gmail.com>
2017-12-10 18:31:38 -0600

    test added to menu suite and new initial entries suite created

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -55,18 +55,31 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function(){
 
+        var body = document.body;
+        var menuIcon = document.querySelector(".menu-icon-link");
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('hidden menu element', function(){
+          expect(body.className).toContain("menu-hidden");
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it("menu displays and hides when clicked", function(){
+          menuIcon.click();
+          expect(body.className).not.toContain("menu-hidden");
+
+          menuIcon.click();
+          expect(body.className).toContain("menu-hidden");
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
